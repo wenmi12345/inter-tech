21718268dd8a1983421ddbd08fd45fa0e1fa8f5e
wenmi12345 <garayzxc20@gmail.com>
2023-08-07 15:54:33 +0330

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        customBlue: "#a2b2c2"
+      }
+    },
+  },
+  plugins: [],
+}
+
